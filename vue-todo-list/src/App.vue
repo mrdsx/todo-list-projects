9dfd9608a8626525660c6397611698493d837cfb
mrdsx <andre0.castlegames@gmail.com>
2025-07-20 10:21:31 +0300

    add form for adding todos

--- vue-todo-list/src/App.vue
+++ vue-todo-list/src/App.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "./components/TodoList.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex h-screen flex-col items-center bg-gray-500 text-center">
+    <div class="h-full pt-[30vh]">
+      <h1 class="text-2xl">Vue Todo List</h1>
+      <form class="my-4 flex justify-center gap-4">
+        <input class="bg-white p-1" type="text" />
+        <button
+          class="bg-orange-400 px-2 hover:bg-orange-500"
+          v-on:click="(e) => e.preventDefault()"
+        >
+          Add Todo
+        </button>
+      </form>
+      <TodoList />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ vue-todo-list/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Todo {
+  content: string;
+}
+
+const todos = ref<Todo[]>([]);
+</script>
+
+<template>
+  <div class="grid gap-2">
+    <div
+      class="flex w-80 items-center justify-between gap-2 rounded-md bg-white p-2"
+      v-for="todo in todos"
+    >
+      <span class="text-start break-all">{{ todo.content }}</span>
+      <button
+        class="max-h-fit self-start bg-red-500 p-1 whitespace-nowrap hover:bg-red-600"
+      >
+        Delete Todo
+      </button>
+    </div>
+  </div>
+</template>
