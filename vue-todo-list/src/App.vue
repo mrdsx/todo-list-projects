6f6abda65e25472a39c2b754a316386c9de12e3a
mrdsx <andre0.castlegames@gmail.com>
2025-07-20 10:59:22 +0300

    add new feature: deleting todo

--- vue-todo-list/src/App.vue
+++ vue-todo-list/src/App.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
+import { ref, useTemplateRef } from "vue";
 import TodoList from "./components/TodoList.vue";
+
+export interface Todo {
+  content: string;
+}
+
+const todos = ref<Todo[]>([]);
+
+const todoInputRef = useTemplateRef("todo-input-ref");
+
+function handleSubmit(e: Event): void {
+  e.preventDefault();
+
+  const todoInputVal = (todoInputRef.value as HTMLInputElement).value;
+  if (todoInputVal.trim().length <= 0) return;
+  addTodo(todoInputVal);
+
+  (todoInputRef.value as HTMLInputElement).value = "";
+}
+
+function addTodo(content: string): void {
+  const newTodo = { content };
+  todos.value.push(newTodo);
+}
 </script>
 
 <template>
   <div class="flex h-screen flex-col items-center bg-gray-500 text-center">
     <div class="h-full pt-[30vh]">
       <h1 class="text-2xl">Vue Todo List</h1>
-      <form class="my-4 flex justify-center gap-4">
-        <input class="bg-white p-1" type="text" />
-        <button
-          class="bg-indigo-400 px-2 hover:bg-indigo-500"
-          v-on:click="(e) => e.preventDefault()"
-        >
-          Add Todo
-        </button>
+      <form class="my-4 flex justify-center gap-4" v-on:submit="handleSubmit">
+        <input class="bg-white p-1" type="text" ref="todo-input-ref" />
+        <button class="bg-indigo-400 px-2 hover:bg-indigo-500">Add Todo</button>
       </form>
-      <TodoList />
+      <TodoList :todos="todos" @update-todos="todos = $event" />
     </div>
   </div>
 </template>
--- vue-todo-list/src/components/TodoList.vue
+++ vue-todo-list/src/components/TodoList.vue
@@ -1,22 +1,26 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import type { Todo } from "../App.vue";
 
-interface Todo {
-  content: string;
+const props = defineProps<{ todos: Todo[] }>();
+const emit = defineEmits(["updateTodos"]);
+
+function deleteTodo(targetTodo: Todo): void {
+  const newTodos = props.todos.filter((todo) => todo !== targetTodo);
+  emit("updateTodos", newTodos);
 }
-
-const todos = ref<Todo[]>([]);
 </script>
 
 <template>
   <div class="grid gap-2">
     <div
       class="flex w-80 items-center justify-between gap-2 rounded-md bg-white p-2"
-      v-for="todo in todos"
+      v-for="(todo, index) in todos"
+      :key="todo.content + index"
     >
       <span class="text-start break-all">{{ todo.content }}</span>
       <button
         class="max-h-fit self-start bg-red-500 p-1 whitespace-nowrap hover:bg-red-600"
+        v-on:click="deleteTodo(todo)"
       >
         Delete Todo
       </button>
