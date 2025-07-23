8beb4ae5b2fdcd699b6a51559c94786d7ece8923
mrdsx <andre0.castlegames@gmail.com>
2025-07-23 14:51:41 +0300

    extract add todo form into component

--- svelte-todo-list/src/App.svelte
+++ svelte-todo-list/src/App.svelte
@@ -1,44 +1,18 @@
 <script lang="ts" module>
+  import AddTodoForm from "./lib/AddTodoForm.svelte";
   import Todos from "./lib/Todos.svelte";
-
-  let todoInput: HTMLInputElement | null = null;
 
   export interface Todo {
     content: string;
   }
 
   let todos = $state<Todo[]>([]);
-
-  function addTodo(e: Event): void {
-    e.preventDefault();
-
-    if (todoInput!.value.trim().length <= 0) return;
-
-    todos.push({ content: todoInput!.value });
-
-    todoInput!.value = "";
-  }
 </script>
 
 <main class="flex h-screen justify-center bg-gray-500">
   <div class="flex flex-col gap-4 text-center">
     <h1 class="mt-40 text-3xl">Svelte Todo List</h1>
-    <form>
-      <input
-        class="bg-white p-1 text-black"
-        id="todo-input"
-        type="text"
-        placeholder="Enter your todo text"
-        bind:this={todoInput}
-      />
-      <button
-        class="bg-orange-400 hover:bg-orange-500"
-        type="submit"
-        onclick={addTodo}
-      >
-        Add Todo
-      </button>
-    </form>
+    <AddTodoForm {todos} />
     <Todos {todos} />
   </div>
 </main>
---
+++ svelte-todo-list/src/lib/AddTodoForm.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import type { Todo } from "../App.svelte";
+
+  interface Props {
+    todos: Todo[];
+  }
+
+  const { todos }: Props = $props();
+
+  let todoInput: HTMLInputElement | null = null;
+
+  function addTodo(e: Event): void {
+    e.preventDefault();
+
+    if (todoInput!.value.trim().length <= 0) return;
+
+    todos.push({ content: todoInput!.value });
+
+    todoInput!.value = "";
+  }
+</script>
+
+<form>
+  <input
+    class="bg-white p-1 text-black"
+    id="todo-input"
+    type="text"
+    placeholder="Enter your todo text"
+    bind:this={todoInput}
+  />
+  <button
+    class="bg-orange-400 hover:bg-orange-500"
+    type="submit"
+    onclick={addTodo}
+  >
+    Add Todo
+  </button>
+</form>
