c49f3b9cac7eb0a6d37046d50137c8f2df80dc41
mrdsx <andre0.castlegames@gmail.com>
2025-07-23 14:56:57 +0300

    add svelte icon in App component

--- svelte-todo-list/src/App.svelte
+++ svelte-todo-list/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" module>
   import AddTodoForm from "./lib/AddTodoForm.svelte";
   import Todos from "./lib/Todos.svelte";
+  import svelteIcon from "./assets/svelte.svg";
 
   export interface Todo {
     content: string;
@@ -11,7 +12,10 @@
 
 <main class="flex h-screen justify-center bg-gray-500">
   <div class="flex flex-col gap-4 text-center">
-    <h1 class="mt-40 text-3xl">Svelte Todo List</h1>
+    <div class="mt-40 flex justify-evenly">
+      <img src={svelteIcon} alt="" />
+      <h1 class="text-3xl">Svelte Todo List</h1>
+    </div>
     <AddTodoForm {todos} />
     <Todos {todos} />
   </div>
