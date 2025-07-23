82565c55afc7e87f048a956cfaa93a92621ce7b9
mrdsx <andre0.castlegames@gmail.com>
2025-07-23 14:41:55 +0300

    add adding and deleting todos & rename Note interface to Todo

--- svelte-todo-list/src/App.svelte
+++ svelte-todo-list/src/App.svelte
@@ -1,33 +1,57 @@
 <script lang="ts">
-  interface Note {
+  let todoInput: HTMLInputElement | null = null;
+
+  interface Todo {
     content: string;
   }
 
-  const notes: Note[] = [{ content: "Note 2" }, { content: "Note 2" }];
+  let todos: Todo[] = $state([]);
+
+  function addTodo(e: Event) {
+    e.preventDefault();
+
+    if (todoInput!.value.trim().length <= 0) return;
+
+    todos.push({ content: todoInput!.value });
+
+    todoInput!.value = "";
+  }
+
+  function deleteTodo(targetTodo: Todo) {
+    todos = todos.filter((todo) => todo !== targetTodo);
+  }
 </script>
 
 <main class="flex h-screen justify-center bg-gray-500">
   <div class="flex flex-col gap-4 text-center">
     <h1 class="mt-40 text-3xl">Svelte Todo List</h1>
-    <div>
+    <form>
       <input
         class="bg-white p-1 text-black"
         id="todo-input"
         type="text"
         placeholder="Enter your todo text"
+        bind:this={todoInput}
       />
-      <button class="bg-orange-400 hover:bg-orange-500">Add Todo</button>
-    </div>
+      <button
+        class="bg-orange-400 hover:bg-orange-500"
+        type="submit"
+        onclick={addTodo}
+      >
+        Add Todo
+      </button>
+    </form>
     <div class="grid gap-2">
-      {#each notes as note}
+      {#each todos as todo}
         <div
           class="flex w-80 items-center justify-between gap-2 rounded-lg bg-white p-2"
         >
-          <div class="text-start break-all">{note.content}</div>
+          <div class="text-start break-all">{todo.content}</div>
           <button
             class="self-start bg-red-500 whitespace-nowrap hover:bg-red-600"
+            onclick={() => deleteTodo(todo)}
           >
-            Delete Todo
+            Delete
           </button>
         </div>
       {/each}
